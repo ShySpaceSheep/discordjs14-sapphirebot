9a2160821365abc5425dad211f6156388a18a3a1
ShySpaceSheep <theminecraftf303@gmail.com>
2023-06-12 17:05:11 +0800

    Initialized project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const { Client, IntentsBitField } = require('discord.js');
+const config = require('./config.json');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ]
+});
+
+client.login(config.token);
+
