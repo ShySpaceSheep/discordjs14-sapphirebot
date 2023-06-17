67e8384d15c12e1afe608aabfc43ce6857be099f
ShySpaceSheep <theminecraftf303@gmail.com>
2023-06-17 18:52:25 +0800

    06/17/23 - 18:51 Commit + Made "temp-register-commands.js" public for viewing + Added more test slash commands (incomplete but it works)

--- src/index.js
+++ src/index.js
@@ -33,6 +33,38 @@ client.on('interactionCreate', (interaction) => {
         const num2 = interaction.options.get('second_number').value;
 
         interaction.reply(`I was given the numbers ${num1} and ${num2}, therefore, their sum would be ${num1 + num2}!`);
+    } else if (cmd === 'helloworld') {
+        const progLangCode = interaction.options.get('programming_language').value;
+
+        // I will change this mouthful of a string, I'll make sure of it.
+        switch (progLangCode) {
+            case 1:
+                interaction.reply("The boilerplate written in C provided below will print out a \`\Hello World\` line in the console window.\n" +
+                                "\`\`\`C\n#include <stdio.h>\n\nint main(int args, char** kwargs) {\n\tprintf(\"Hello World!\");\n\treturn 0;\n}\`\`\`" +
+                                "\nTo further explain the given source code, we've included the \`stdio.h\` library to import standard I/O functions which will be relevant with our use of the \`printf()\` function to print out our \`Hello World\` line." +
+                                "\n\nOur \`printf()\` is tucked away in a function called \`main()\` where our program will always execute first and foremost when we launch it. A successful runtime instance will yield an integer of 0 as indicated by the \`return 0;\` line, anything else will signify an unsuccessful termination." +
+                                "\n\nThis is generally almost the same as the source code for the same task in C++ as they share many elements and syntax.");
+                break;
+            case 2:
+                interaction.reply("The boilerplate written in C++ provided below will print out a \`\Hello World\` line in the console window.\n" +
+                                "\`\`\`C++\n#include <iostream>\n\nint main(int args, char** kwargs) {\n\tstd::cout << \"Hello World!\" << std::endl;\n\treturn 0;\n}\`\`\`" +
+                                "\n To further explain the given source code, we've included the \`iostream\` namespace to import standard I/O functions which contains the `cout` and `endl` functions that we'll be using to print out our \'Hello World\' line." +
+                                "\n\nOur \`cout\` and \'endl\' functions are tucked away in a function called \`main()\` where our program will always execute first and foremost when we launch it. A successful runtime instance will yield an integer of 0 as indicated by the \`return 0;\` line, anything else will signify an unsuccessful termination." +
+                                "\n\nThis is generally almost the same as the source code for the same task in C as they share many elements and syntax.");
+                break;
+            case 3:
+                interaction.reply("\`\`\`Java\npublic class Main {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println(\"Hello World!\");\n\t}\n}\`\`\`");
+                break;
+            case 4:
+                interaction.reply("It is fairly simple to print out a \'Hello World\' line using Javascript using the following line of code below:\n" +
+                                "\`\`\`Javascript\nconsole.log(\"Hello World!\");\`\`\`");
+                break;
+            case 5:
+                interaction.reply("\`\`\`Python\nprint(\"Hello World!\");\`\`\`");
+                break;
+            default:
+                break;
+        }
     }
 });
 
---
+++ src/temp-register-commands.js
@@ -0,0 +1,59 @@
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
+const config = require('./config.json');
+
+const commands = [
+    { name: 'pekoify', description: 'Append Pekora\'s \"peko\" to a message!' },
+    { name: 'now', description: 'Retrieve the current date and time.'},
+    { 
+        name: 'add',
+        description: 'Adds two numbers provided by the user',
+        options: [
+            {
+                name: 'first_number',
+                description: 'The base number of the equation.',
+                type: ApplicationCommandOptionType.Number,
+                required: true
+            }, 
+            {
+                name: 'second_number',
+                description: 'The number that\'s added to the base number.',
+                type: ApplicationCommandOptionType.Number,
+                required: true
+            }
+        ]
+    },
+    {
+        name: 'helloworld',
+        description: 'Writes the equivalent hello world source code of the chosen programming language',
+        options: [
+            {
+                name: 'programming_language',
+                description: 'The name of the programming language we want to write in.',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+                choices: [
+                    { name: 'C', value: 1 },
+                    { name: 'C++', value: 2 },
+                    { name: 'Java', value: 3 },
+                    { name: 'Javascript', value: 4 },
+                    { name: 'Python', value: 5 }
+                ]
+            }
+        ]
+    }
+];
+
+const rest = new REST({version: '10'}).setToken(config.token);
+
+(async () => {
+    try {
+        console.log("\u001b[1;33m[STATUS] Registering all available slash commands...", "\u001b[0m");
+        await rest.put(
+            Routes.applicationGuildCommands(config.clientID, config.homeGuildID),
+            { body: commands }
+        );
+        console.log("\u001b[1;32m[STATUS] Success!", "\u001b[0m");
+    } catch (error) {
+        console.log(`\u001b[1;31m[STATUS] Runtime error encountered: ${error}...`, "\u001b[0m");
+    }
+})();
