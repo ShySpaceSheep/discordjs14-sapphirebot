6e1d15dd63b540d8c74b3674ab2e537ebccb4784
ShySpaceSheep <theminecraftf303@gmail.com>
2023-06-20 17:01:23 +0800

    06/20/23 - 17:00 Commit - Removed all concurrent test commands for cleanup and command list finalization

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { Client, IntentsBitField, EmbedBuilder, ActivityType } = require('discord.js');
+const { Client, IntentsBitField, EmbedBuilder, ActivityType, Guild } = require('discord.js');
 const config = require('./config.json');
 
 const client = new Client({
@@ -19,70 +19,28 @@ client.on('ready', (c) => {
     }
 });
 
+// TODO: Finalize available command list
 client.on('interactionCreate', (interaction) => {
     if (!interaction.isChatInputCommand()) { return; }
     const cmd = interaction.commandName;
 
-    if (cmd === 'pekoify') {
-        interaction.reply("Konpeko konpeko konpeko! Hololive sankisen no Usadaaaa Pekora-peko!");
-    } else if (cmd === 'now') {
-        const currentTimestamp = new Date();
-        interaction.reply(currentTimestamp.toString());
-    } else if (cmd === 'add') {
-        const num1 = interaction.options.get('first_number').value;
-        const num2 = interaction.options.get('second_number').value;
-
-        interaction.reply(`I was given the numbers ${num1} and ${num2}, therefore, their sum would be ${num1 + num2}!`);
-    } else if (cmd === 'me') {
+    if (cmd === 'profile') {
         const embed = new EmbedBuilder()
-        .setAuthor({ name: "\u200B", iconURL: interaction.user.avatarURL() })
+        //.setAuthor({ name: `${interaction.user.username}'s Stats`, iconURL: interaction.user.avatarURL() })
         .setColor("Aqua")
         .setThumbnail(interaction.user.avatarURL())
-        .setTitle("You have called for an embed!")
-        .setDescription(`${interaction.user.username} has initiated the embed command!`)
+        .setTitle("Server Profile Statistics")
+        .setDescription(`Hello there, \`${interaction.user.username}\`! You are currently looking at your profile stats for the server \`${interaction.guild.name}\`!`)
         .addFields(
-            { name: "⭐ Reputation", value: "8.67/10", inline: true },
-            { name: "🏆 Current Rank", value: "**TOP 4**", inline: true },
-            { name: "👏 Appraisals Received", value: "21", inline: true },
+            { name: "⭐ Reputation", value: "***8.67/10***", inline: true },
+            { name: "👏 Applauses Received", value: "***21 Awarded***", inline: true },
+            { name: "🏆 Current Rank", value: "***TOP 4***", inline: true }
         )
-        .setImage("https://media.tenor.com/7XwcFNembSoAAAAC/bocchi-the-rock-hitori-gotoh.gif")
+        //.setImage("https://media.tenor.com/7XwcFNembSoAAAAC/bocchi-the-rock-hitori-gotoh.gif")
         .setFooter({ text: "Embed created using Sapphire", iconURL: client.user.avatarURL() })
         .setTimestamp();
 
         interaction.reply({ embeds: [embed] });
-    } else if (cmd === 'helloworld') {
-        const progLangCode = interaction.options.get('programming_language').value;
-
-        // I will change this mouthful of a string, I'll make sure of it.
-        switch (progLangCode) {
-            case 1:
-                interaction.reply("The boilerplate written in C provided below will print out a \`\Hello World\` line in the console window.\n" +
-                                "\`\`\`C\n#include <stdio.h>\n\nint main(int args, char** kwargs) {\n\tprintf(\"Hello World!\");\n\treturn 0;\n}\`\`\`" +
-                                "\nTo further explain the given source code, we've included the \`stdio.h\` library to import standard I/O functions which will be relevant with our use of the \`printf()\` function to print out our \`Hello World\` line." +
-                                "\n\nOur \`printf()\` is tucked away in a function called \`main()\` where our program will always execute first and foremost when we launch it. A successful runtime instance will yield an integer of 0 as indicated by the \`return 0;\` line, anything else will signify an unsuccessful termination." +
-                                "\n\nThis is generally almost the same as the source code for the same task in C++ as they share many elements and syntax.");
-                break;
-            case 2:
-                interaction.reply("The boilerplate written in C++ provided below will print out a \`\Hello World\` line in the console window.\n" +
-                                "\`\`\`C++\n#include <iostream>\n\nint main(int args, char** kwargs) {\n\tstd::cout << \"Hello World!\" << std::endl;\n\treturn 0;\n}\`\`\`" +
-                                "\n To further explain the given source code, we've included the \`iostream\` namespace to import standard I/O functions which contains the `cout` and `endl` functions that we'll be using to print out our \'Hello World\' line." +
-                                "\n\nOur \`cout\` and \'endl\' functions are tucked away in a function called \`main()\` where our program will always execute first and foremost when we launch it. A successful runtime instance will yield an integer of 0 as indicated by the \`return 0;\` line, anything else will signify an unsuccessful termination." +
-                                "\n\nThis is generally almost the same as the source code for the same task in C as they share many elements and syntax.");
-                break;
-            case 3:
-                interaction.reply("\`\`\`Java\npublic class Main {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println(\"Hello World!\");\n\t}\n}\`\`\`");
-                break;
-            case 4:
-                interaction.reply("As with Python, it is fairly simple to print out a \'Hello World\' line with Javascript using the following line of code below:\n" +
-                                "\`\`\`Javascript\nconsole.log(\"Hello World!\");\`\`\`");
-                break;
-            case 5:
-                interaction.reply("As with Javascript, it is fairly simple to print out a \'Hello World\' line with Python using the following line of code below:\n" +
-                                "\`\`\`Python\nprint(\"Hello World!\");\`\`\`");
-                break;
-            default:
-                break;
-        }
     }
 });
 
--- src/temp-register-commands.js
+++ src/temp-register-commands.js
@@ -2,46 +2,7 @@ const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
 const config = require('./config.json');
 
 const commands = [
-    { name: 'pekoify', description: 'Append Pekora\'s \"peko\" to a message!' },
-    { name: 'now', description: 'Retrieve the current date and time.'},
-    { name: 'me', description: 'Show the user their public Discord information.' },
-    { 
-        name: 'add',
-        description: 'Adds two numbers provided by the user',
-        options: [
-            {
-                name: 'first_number',
-                description: 'The base number of the equation.',
-                type: ApplicationCommandOptionType.Number,
-                required: true
-            }, 
-            {
-                name: 'second_number',
-                description: 'The number that\'s added to the base number.',
-                type: ApplicationCommandOptionType.Number,
-                required: true
-            }
-        ]
-    },
-    {
-        name: 'helloworld',
-        description: 'Writes the equivalent hello world source code of the chosen programming language',
-        options: [
-            {
-                name: 'programming_language',
-                description: 'The name of the programming language we want to write in.',
-                type: ApplicationCommandOptionType.Number,
-                required: true,
-                choices: [
-                    { name: 'C', value: 1 },
-                    { name: 'C++', value: 2 },
-                    { name: 'Java', value: 3 },
-                    { name: 'Javascript', value: 4 },
-                    { name: 'Python', value: 5 }
-                ]
-            }
-        ]
-    }
+    { name: 'profile', description: 'Display a detailed view of your server profile statistics.' }
 ];
 
 const rest = new REST({version: '10'}).setToken(config.token);
