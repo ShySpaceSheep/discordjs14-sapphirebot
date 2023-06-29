fd3dbd608c447f695ace16de15e803589a85fa07
ShySpaceSheep <theminecraftf303@gmail.com>
2023-06-29 11:21:09 +0800

    06/29/23 - 11:20 + Added command handler to actually process slash commands

--- src/events/interactionCreate/cmdHandler.js
+++ src/events/interactionCreate/cmdHandler.js
@@ -1,5 +1,14 @@
+const { homeGuildID } = require('../../config.json');
 const retrieveLocalCmds = require('../../utils/retrieveLocalCommamds');
 
-module.exports = (client, interaction) => {
-
+module.exports = async (client, interaction) => {
+    const localCmds = retrieveLocalCmds();
+    if(interaction.isChatInputCommand()) {
+        try {
+            const cmdObj = localCmds.find((cmd) => cmd.name === interaction.commandName);
+            await cmdObj.callback(client, interaction);
+        } catch (error) {
+            console.log("ERROR");
+        }
+    }
 };
